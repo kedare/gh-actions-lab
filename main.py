7466d06a0f0b265f695c6cbc76a016961f898eb8
Mathieu Poussin <mathieu.poussin@cardiologs.com>
2022-09-29 14:08:09 +0200

    Add test python code

--- main.py
+++ main.py
@@ -13,4 +13,8 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+
+def ThisShouldFail():
+    print("ugly python")
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
